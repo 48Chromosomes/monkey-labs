a8be45d4f6a14ca3a6e459b6a9b9c13618a64643
Marc Horne <me@marcushorne.com>
2023-04-10 16:21:45 +0100

    feat: add info card to toggle switch

--- components/Actions/ToggleListener/ToggleListener.tsx
+++ components/Actions/ToggleListener/ToggleListener.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState, MouseEventHandler } from 'react';
+import React from 'react';
 import * as Switch from '@radix-ui/react-switch';
 
 import styles from './ToggleListener.module.scss';
 
 import { useAppStore } from '@/stores/AppStore';
+
+import InfoCard from '@/components/InfoCard/InfoCard';
 
 export default function ToggleListener() {
   const { toggleListener } = useAppStore();
@@ -12,6 +14,7 @@ export default function ToggleListener() {
     <div className={styles.switchContainer}>
       <label className={styles.label} htmlFor='listener'>
         Toggle Listener
+        <InfoCard>Hold down the microphone icon in the console to use speech-to-text input</InfoCard>
       </label>
       <Switch.Root className={styles.switchRoot} id='listener' onCheckedChange={toggleListener}>
         <Switch.Thumb className={styles.switchThumb} />
---
+++ components/InfoCard/InfoCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import * as HoverCard from '@radix-ui/react-hover-card';
+import { InfoCircledIcon } from '@radix-ui/react-icons';
+
+import styles from './InfoCard.module.scss';
+
+export default function InfoCard({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={styles.infoCardContainer}>
+      <HoverCard.Root>
+        <HoverCard.Trigger className={styles.hoverCardTrigger} asChild>
+          <InfoCircledIcon />
+        </HoverCard.Trigger>
+
+        <HoverCard.Portal>
+          <HoverCard.Content className={styles.hoverCardContent}>{children}</HoverCard.Content>
+        </HoverCard.Portal>
+      </HoverCard.Root>
+    </div>
+  );
+}
