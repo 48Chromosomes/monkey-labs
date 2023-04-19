aa5f4ee6f8f9eb09e43be9d66f7684294b2fba86
Marc Horne <me@marcushorne.com>
2023-04-19 16:17:27 +0100

    feat: return 4 documents for prompt

--- utilities/pinecone/makechain.ts
+++ utilities/pinecone/makechain.ts
@@ -41,6 +41,6 @@ export const makeChain = (
     combineDocumentsChain: docChain,
     questionGeneratorChain: questionGenerator,
     returnSourceDocuments: true,
-    k: 2, //number of source documents to return
+    k: 4, //number of source documents to return
   });
 };
