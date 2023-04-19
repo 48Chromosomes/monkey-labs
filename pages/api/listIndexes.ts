e46304c074e8ae98d285792dcca238a163c77784
Marc Horne <me@marcushorne.com>
2023-04-19 11:28:51 +0100

    fix: use path for listindexes

--- pages/api/listIndexes.ts
+++ pages/api/listIndexes.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { initPinecone } from '@/utilities/pinecone/pinecone-client';
 import fs from 'fs';
+import path from 'path';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.body.currentVectorStore === 'Pinecone') {
@@ -10,7 +11,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     res.status(200).json(indexesList);
   } else if (req.body.currentVectorStore === 'HNSWLib') {
-    fs.readdir('./vectors', (err, files) => {
+    fs.readdir(path.join(__dirname, '/vectors'), (err, files) => {
       const indexesList: string[] = [];
 
       files.forEach((file) => indexesList.push(file.split('.')[0]));
